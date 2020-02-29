9b2381c4deccb079806edcd1c5cb91678dd1df17
steniossn <51008149+steniossn@users.noreply.github.com>
2020-02-29 00:59:50 -0300

    prototipo

---
+++ src/condominio/fx/FXMLEntradaController.java
@@ -0,0 +1,253 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condominio.fx;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import javax.swing.JOptionPane;
+import util.Variaveis;
+
+/**
+ * FXML Controller class
+ *
+ * @author portaria
+ */
+public class FXMLEntradaController implements Initializable {
+
+    @FXML
+    private TextField tfEntrada;
+
+    @FXML
+    private TextField tfCasa;
+
+    @FXML
+    private Button btnOk;
+
+    @FXML
+    private Label ID;
+        
+    @FXML
+    private CheckBox cbTrazerVisita;
+    
+    @FXML
+    private CheckBox cbBuscarVisita;
+    
+    @FXML
+    private TextField tfMotorista;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private TextField tfPlaca;
+
+    @FXML
+    private TextField tfNome;
+    
+    @FXML
+    private ComboBox combBVisita;
+    
+    
+    
+    //***************variaveis****************
+    
+    int iD = 0;
+    
+
+    public FXMLEntradaController() {
+    }
+    //int count = -1;
+    //String letra;
+    //public static ArrayList<String> digitado = new ArrayList();
+   
+    
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+       
+        
+        //coloca hora do sistema em entrada assim que a janela é aberta
+        tfEntrada.setText(Variaveis.DataHora("hora"));
+        
+        // ao apertar ok pegar o nome informado e acionar metodo 
+        btnOk.setOnAction((ActionEvent event) -> {
+            if (cbTrazerVisita.isSelected()) {
+                tfNome.setText(inicialMaiuscula(tfNome.getText()));
+                visitaDeUber();
+            } else {
+                tfNome.setText(inicialMaiuscula(tfNome.getText()));
+                entrada();
+            }
+
+            tfCasa.requestFocus();
+            
+            //fechar a tela
+            Stage stage = (Stage) btnOk.getScene().getWindow(); //pegar tela atual
+            stage.close(); //fechar
+        });
+
+        //ao apertar botão de cancelar fechar a janela
+        btnCancelar.setOnAction((event) -> {
+            FXMLDocumentController.RAIZ.preencherJCombobox();
+            //finalizar janela
+            Stage stage = (Stage) btnCancelar.getScene().getWindow(); //pegar tela atual
+            stage.close(); //fechar
+            
+        });
+
+        //ao apertar enter trocar o foco 
+        tfCasa.setOnAction((event) -> {
+            tfNome.requestFocus();
+        });
+
+        //ao apertar enter trocar o foco 
+        tfNome.setOnAction((event) -> {
+            tfPlaca.requestFocus();
+        });
+
+        //ao apertar enter trocar o foco 
+        tfNome.setOnKeyPressed((event) -> {
+            String texto = tfNome.getText();
+
+            if (event.getCode() == KeyCode.ENTER & !texto.isEmpty()) {
+                tfNome.setText(inicialMaiuscula(texto));
+            }
+        });
+
+        
+        tfPlaca.setOnAction((event) -> {
+            //salva e fecha
+            entrada();
+             //finalizar janela
+            Stage stage = (Stage) btnCancelar.getScene().getWindow(); //pegar tela atual
+            stage.close(); //fechar
+            
+        });
+
+
+        tfPlaca.setOnKeyTyped((event) -> {
+            String letra = event.getCharacter();
+            String letrasPlaca = "\\D{3}";
+            String padrao = "\\D{3}-\\d{4}";
+
+            String texto = tfPlaca.getText();
+            if (texto.matches(letrasPlaca)) {
+                tfPlaca.setText(texto.toUpperCase(Locale.ROOT) + "-");
+               //coloca o caracter na ultima posição
+                tfPlaca.end();
+            }
+            if (texto.matches(padrao)) {
+                event.consume();
+            }
+
+        });
+    }
+
+
+
+    //capiturar os dados e salvar em txt
+    public void entrada() {
+        try {
+
+            byte[] bytes = Files.readAllBytes(Variaveis.relatorio());
+            String relatorio = new String(bytes);
+
+            ArrayList<String> entrada = new ArrayList<>();
+            entrada.add(0, relatorio);
+            entrada.add(1, "Casa: " + tfCasa.getText());
+            entrada.add(2, "Nome: " + tfNome.getText());
+            entrada.add(3, "Placa: " + tfPlaca.getText().toUpperCase(Locale.ROOT));
+            entrada.add(4, "Entrada: " + tfEntrada.getText());
+            entrada.add(5, "Saida:");
+            Files.write(Variaveis.relatorio(), entrada, Charset.defaultCharset());
+            
+            //metodo de outra classe que carrega um combobox 
+            FXMLDocumentController.RAIZ.preencherJCombobox();
+
+        } catch (IOException ex) {
+            alerta("erro","não foi possivel registrar os dados" );
+            //JOptionPane.showMessageDialog(null, "não foi possivel registrar os dados");
+        }
+
+    }
+
+    //add o visitante que veio de uber 
+    public void visitaDeUber() {
+        try {
+
+            byte[] bytes = Files.readAllBytes(Variaveis.relatorio());
+            String relatorio = new String(bytes);
+
+            ArrayList<String> entrada = new ArrayList<>();
+            //indice 0 contem os dados ja existentes no arquivo de entrada
+            entrada.add(0, relatorio);
+            //aqui sera adicionado os dados inseridos pelo usuario
+            entrada.add(1, "Casa: " + tfCasa.getText());
+            entrada.add(2, "Nome: Uber " + tfMotorista.getText());
+            entrada.add(3, "Placa: " + tfPlaca.getText().toUpperCase(Locale.ROOT));
+            entrada.add(4, "Entrada: " + tfEntrada.getText());
+            entrada.add(5, "Saida:");
+            entrada.add(6, "");
+            //aqui sera criado nova entrada para o passageiro do uber 
+            entrada.add(7, "Casa: " + tfCasa.getText());
+            entrada.add(8, "Nome: " + tfNome.getText() + " veio de uber");
+            entrada.add(9, "Placa: " + " esta sem carro");
+            entrada.add(10, "Entrada: " + tfEntrada.getText());
+            entrada.add(11, "Saida:");
+
+            //escreve em um arquivo
+            Files.write(Variaveis.relatorio(), entrada, Charset.defaultCharset());
+            //metodo de outra classe que carrega um combobox
+            FXMLDocumentController.RAIZ.preencherJCombobox();
+
+        } catch (IOException ex) {
+            alerta("erro","não foi possivel registrar os dados" );
+           // JOptionPane.showMessageDialog(null, "não foi possivel registrar os dados");
+        }
+    }
+
+    public String inicialMaiuscula(String string) {
+        ArrayList nome = new ArrayList();
+        String[] split = string.split("\\s");
+        for (String texto : split) {
+            String textoMod = texto.toUpperCase().charAt(0) + texto.substring(1, texto.length());
+            nome.add(textoMod);
+        }
+        return nome.toString().replace("[", "").replace("]", "").replaceAll(",", "");
+    }
+    
+    public void alerta(String title , String text ){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(text);
+        alert.setContentText(null);
+        alert.show();     
+        
+    }
+    
+    
+    
+}
---
+++ src/util/Variaveis.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.time.Month;
+import java.util.Calendar;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author portaria
+ */
+public class Variaveis {
+
+    //Paths
+    //Paths
+    public static Path PASTAPRINCIPAL = Paths.get(RecuperaRaiz() + "CondominioFx/Dados/" + FileSystems.getDefault().getSeparator());
+    public static Path PASTADADOSAPP = Paths.get(RecuperaRaiz() + "CondominioFx/Dados/DadosApp" + FileSystems.getDefault().getSeparator());
+    public static Path PASTARELATORIO = Paths.get(RecuperaRaiz() + "CondominioFx/Dados/Relatorio/" + FileSystems.getDefault().getSeparator());
+    public static Path CASAS = Paths.get(RecuperaRaiz() + "CondominioFx/Dados/Casas" + FileSystems.getDefault().getSeparator());
+    public static Path SERVICOS = Paths.get(RecuperaRaiz() + "CondominioFx/Dados/Servicos/" + FileSystems.getDefault().getSeparator());
+
+   
+    public static String RecuperaRaiz() {
+
+        Iterable<Path> dirs = FileSystems.getDefault().getRootDirectories();// 
+        for (Path path : dirs) { // for percorre todos os diretorios armazenados no array path
+            return path.toString();
+        }
+        return null;
+    }
+
+    //retorna o Paths de relatorio C:\Condominio\Dados\Relatorio\ + nome do arquivo
+    public static Path relatorio() {
+        String[] lbl = DataHora("data").split("/");
+        int d = Integer.parseInt(lbl[0]);
+        int m = Integer.parseInt(lbl[1]);
+        int a = Integer.parseInt(lbl[2]);
+        // essa nova string é criada para que o zero a esquerda seja retirado da data
+        String dataConvertida = String.valueOf(String.valueOf(Integer.toString(d) + ".") + Integer.toString(m) + ".") + Integer.toString(a);
+
+        Path relatorioF = Paths.get(PASTARELATORIO + FileSystems.getDefault().getSeparator() + dataConvertida + ".txt");
+        return relatorioF;
+
+    }
+
+    //cria as pastas do programa
+    public static void CriarPastas() {
+
+        try {
+
+            //cria a pasta principal "Condominio" e a pasta "Dados"
+            if (!Files.exists(PASTAPRINCIPAL)) {
+                Files.createDirectories(PASTAPRINCIPAL);
+            }
+            //cria a pasta "relatorio"
+            if (!Files.exists(PASTADADOSAPP)) {
+                Files.createDirectory(PASTADADOSAPP);
+            }
+            //cria a pasta "DadosApp"
+            if (!Files.exists(PASTARELATORIO)) {
+                Files.createDirectory(PASTARELATORIO);
+            }
+            if (!Files.exists(CASAS)) {
+                Files.createDirectory(CASAS);
+            }
+            if (!Files.exists(SERVICOS)) {
+                Files.createDirectory(SERVICOS);
+            }
+
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "pastas do programa não encontradas");
+        }
+
+    }
+
+    public static String DataHora(String string) {
+        Calendar c1 = Calendar.getInstance();
+        DateFormat formataData = DateFormat.getDateInstance();
+        String data = formataData.format(c1.getTime());
+        DateFormat formathora = DateFormat.getTimeInstance();
+        String hora = formathora.format(c1.getTime());
+        int apenasHora = c1.get(Calendar.HOUR_OF_DAY);
+        int minutos = c1.get(Calendar.MINUTE);
+
+        switch (string) {
+            case "hora":
+                return hora;
+            case "data":
+                return data;
+            case "apenasHora":
+                return Integer.toString(apenasHora);
+            case "minutos":
+                return Integer.toString(minutos);
+
+        }
+        return null;
+    }
+
+    /**
+     * recebe uma data e gera uma string com uma data que pode ser usada no
+     * programa
+     *
+     * @param data
+     * @return String com data modificada
+     */
+    public static String mudaDatas(String data) {
+        String[] dataRecebida = data.split("/");
+        int d = Integer.parseInt(dataRecebida[0]);
+        int m = Integer.parseInt(dataRecebida[1]);
+        int a = Integer.parseInt(dataRecebida[2]);
+
+        String novaData = Integer.toString(d) + "." + Integer.toString(m) + "." + Integer.toString(a);
+
+        return novaData;
+    }
+
+    public static Month verificaMes(int mes) {
+        if (mes > 12) {
+            return null;
+        }
+        Month[] month = {null, Month.JANUARY, Month.FEBRUARY, Month.MARCH,
+            Month.APRIL, Month.MAY, Month.JUNE, Month.JULY,
+            Month.AUGUST, Month.SEPTEMBER, Month.OCTOBER,
+            Month.NOVEMBER, Month.DECEMBER};
+
+        return month[mes];
+
+    }
+
+    /**
+     * *
+     * @param arquivo local do arquivo a ser lido
+     * @return uma String dos bytes lidos do arquivo informado
+     */
+    public static String lerString(String arquivo) {
+        try {
+            // .../Condominio/Dados/arquivo 
+            Path path = Paths.get(PASTAPRINCIPAL + FileSystems.getDefault().getSeparator() + arquivo); //CAMIMNHO
+
+            if (Files.exists(path)) {
+                byte[] bytes = Files.readAllBytes(path); // ARRAY DE BYTE QUE RECEBE OS DADOS DO ARQUIVO DE TXT
+                String txtbytes = new String(bytes); // PASSA O ARRAY DE BYTE PARA STRING
+                return txtbytes;
+            } else {
+                return null;
+            }
+
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Erro de leitura do arquivo ou arquivo nao existe");
+
+        }
+        return null;
+    }
+
+    public static Image iconPadrao() {
+        URL url = Variaveis.class.getResource("/imagens/icon/casa.png");
+        Image iconeTitulo = Toolkit.getDefaultToolkit().getImage(url);
+        return iconeTitulo;
+    }
+ 
+}
